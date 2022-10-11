20d1683f9a32a2b81612b0636c7e7a5c2bfb6807
Shinryuzz <@@>
2022-10-11 14:23:23 +0900

    init

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,13 @@
+hs.loadSpoon("ShiftIt")
+spoon.ShiftIt:bindHotkeys({
+  maximum = { { 'ctrl','cmd' }, 'm' },
+  toggleFullScreen = { { 'ctrl', 'cmd' }, 'f' },
+  left = { { 'ctrl', 'cmd' }, 'h' },
+  left = { { 'ctrl', 'cmd' }, 'left' },
+  right = { { 'ctrl', 'cmd' }, 'right' },
+  center = { { 'ctrl', 'cmd' }, 'o' },
+  resizeOut = { { 'ctrl', 'cmd' }, '=' },
+  resizeIn = { { 'ctrl', 'cmd' }, '-' }
+
+
+})
