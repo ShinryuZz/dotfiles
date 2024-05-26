243aa033b39229b86247b229bed6688d4e38d6c7
Shinryuzz <mshin1127@gmail.com>
2024-05-26 23:51:15 +0900

    update keymappings for hammerspoon

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,23 +1,24 @@
 hs.loadSpoon("ShiftIt")
 
-mash = {'ctrl', 'cmd'}
+mash = { "ctrl", "cmd" }
+
 myKeys = {
-  left = { mash, 'left' },
-  right = { mash, 'right' },
-  up = { mash, 'up' },
-  down = { mash, 'down' },
-  upleft = { mash, '1' },
-  upright = { mash, '2' },
-  botleft = { mash, '3' },
-  botright = { mash, '4' },
-  maximum = { mash, 'm' },
-  toggleFullScreen = { mash, 'f' },
-  toggleZoom = { mash, 'z' },
-  center = { mash, 'o' },
-  nextScreen = { mash, 'l' },
-  previousScreen = { mash, 'h' },
-  resizeOut = { mash, '=' },
-  resizeIn = { mash, '-' },
+	left = { mash, "h" },
+	right = { mash, "l" },
+	up = { mash, "up" },
+	down = { mash, "down" },
+	upleft = { mash, "1" },
+	upright = { mash, "2" },
+	botleft = { mash, "3" },
+	botright = { mash, "4" },
+	maximum = { mash, "m" },
+	toggleFullScreen = { mash, "f" },
+	toggleZoom = { mash, "z" },
+	center = { mash, "o" },
+	nextScreen = { mash, "n" },
+	previousScreen = { mash, "p" },
+	resizeOut = { mash, "=" },
+	resizeIn = { mash, "-" },
 }
 
 spoon.ShiftIt:bindHotkeys(myKeys)
